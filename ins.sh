93a1885d39bc8a4b182441e1d418297377adecbd
Bobi Saputra <40152293+bobisaputra@users.noreply.github.com>
2020-09-20 20:44:57 +0700

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -1,8 +1,9 @@
 add-apt-repository ppa:ondrej/php
 apt update
-apt install php7.3 php7.3-cli php7.3-common
-apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl
-update-alternatives --set php /usr/bin/php7.3
+apt install php7.4 php7.4-cli php7.4-common
+apt install php-pear php7.4-curl php7.4-dev php7.4-gd php7.4-mbstring php7.4-zip php7.4-mysql php7.4-xml php7.4-fpm libapache2-mod-php7.4 php7.4-imagick php7.3-recode php7.4-tidy php7.4-xmlrpc php7.4-intl
+update-alternatives --set php /usr/bin/php7.4
 a2dismod php7.0
 a2dismod php7.2
+a2dismod php7.3
 a2enmod php7.3
