27e818268c4ddfb5c3316cc93c7803ac177a2c14
Kang Bobi <40152293+kangbobi@users.noreply.github.com>
2021-02-05 09:14:15 +0700

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -1,3 +1,4 @@
+#ubuntu
 add-apt-repository ppa:ondrej/php
 apt update
 apt install php7.4 php7.4-cli php7.4-common
@@ -7,3 +8,15 @@ a2dismod php7.0
 a2dismod php7.2
 a2dismod php7.3
 a2enmod php7.4
+
+#centos
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+
+#install a collection of useful programs for managing yum repositories and packages
+yum install yum-utils
+
+# One of the programs provided by yum-utils is yum-config-manager
+yum-config-manager --enable remi-php73
+
+yum install php php-pear php-ldap php-cli php-mcrypt php-curl php-dev php-gd php-mbstring php-zip php-mysql php-xml php-fpm libapache2-mod-php php-imagick php-recode php-tidy php-xmlrpc php-intl
